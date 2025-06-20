e6dbd19ada5f79671d62a31a0a931ecead91fbde
JuanBuitrago <juanbuitrago@periferia-it.com>
2025-06-20 01:39:45 -0500

    Modificar APP

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,21 @@
   <v-app>
     <v-main>
       <v-container>
-        <v-btn color="primary" icon="mdi-home">Prueba</v-btn>
+        <SearchBar v-model="searchQuery" />
+        <UserList :search="searchQuery" @show-user="openUserModal" />
+        <UserModal v-if="selectedUser" :user="selectedUser" @close="selectedUser = null" />
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import SearchBar from './components/SearchBar.vue'
+import UserList from './components/UserList.vue'
+import UserModal from './components/UserModal.vue'
+
+const searchQuery = ref('')
+const selectedUser = ref(null)
+const openUserModal = (user) => selectedUser.value = user
 </script>
